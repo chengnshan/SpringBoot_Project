183f945d74d1ba6789915dc49a29dbe0fb68b144
cheng <276629352@qq.com>
2019-06-23 21:34:19 +0800

    springboot2中使用websocket

---
+++ springboot2-websocket/src/test/java/com/cxp/springboot2websocket/Springboot2WebsocketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cxp.springboot2websocket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot2WebsocketApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
