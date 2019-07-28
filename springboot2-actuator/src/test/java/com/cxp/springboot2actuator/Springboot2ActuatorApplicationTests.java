18b5896ab39336802a441ab7de4d7ca2ad2783d9
cheng <276629352@qq.com>
2019-07-28 11:17:43 +0800

    springboot2中使用Actuator提供对应用系统的自省和监控的集成功能

---
+++ springboot2-actuator/src/test/java/com/cxp/springboot2actuator/Springboot2ActuatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cxp.springboot2actuator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot2ActuatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
