dc61fa2f43e71060515d50b96c51359dbb34ced7
cheng <276629352@qq.com>
2019-08-03 09:32:49 +0800

    springboot2项目使用zookeeper

---
+++ springboot2-zookeeper/src/main/java/com/cxp/springboot2zookeeper/CuratorBase.java
@@ -0,0 +1,71 @@
+package com.cxp.springboot2zookeeper;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.GetChildrenBuilder;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+/**
+ * @author 程
+ * @date 2019/7/29 下午3:40
+ */
+public class CuratorBase {
+
+    /** zookeeper地址 */
+    static final String CONNECT_ADDR = "192.168.111.134:2181";
+    /** session超时时间  ms */
+    static final int SESSION_OUTTIME = 5000;
+
+    public static void main(String[] args) throws Exception {
+//1 重试策略：初试时间为1s 重试10次
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 2);
+        //2 通过工厂创建连接
+        CuratorFramework cf = CuratorFrameworkFactory.builder()
+                .connectString(CONNECT_ADDR)
+                .sessionTimeoutMs(SESSION_OUTTIME)
+                .retryPolicy(retryPolicy)
+//					.namespace("super")
+                .build();
+        //3 开启连接
+        cf.start();
+
+        System.out.println(ZooKeeper.States.CONNECTED);
+        System.out.println(cf.getState());
+
+        // 新加、删除
+
+        //4 建立节点 指定节点类型（不加withMode默认为持久类型节点）、路径、数据内容
+/*        String result1 = cf.create().creatingParentsIfNeeded()
+                .withMode(CreateMode.PERSISTENT)
+                .forPath("/zookeeper/super/c1","c1内容".getBytes());
+        System.out.println(result1);*/
+        //5 删除节点
+        cf.delete().guaranteed().deletingChildrenIfNeeded().forPath("/super/c2");
+        cf.delete().guaranteed().deletingChildrenIfNeeded().forPath("/");
+
+        cf.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT)
+                .forPath("/super/c2","c2内容".getBytes());
+        Stat stat = cf.checkExists().forPath("/super/c2");
+        System.out.println(stat.toString());
+        //读取节点
+        String ret1 = new String(cf.getData().forPath("/super/c2"));
+        System.out.println(ret1);
+        //修改节点
+        cf.setData().forPath("/super/c2", "修改c2内容c2内容c2内容".getBytes());
+        String ret2 = new String(cf.getData().forPath("/super/c2"));
+        System.out.println(ret2);
+
+        GetChildrenBuilder children = cf.getChildren();
+        List<String> strings = children.forPath("/kafka");
+        System.out.println(strings);
+
+        cf.close();
+    }
+}
