627f3c586cc1cb03328cc8dee079ac8206cffdcf
cheng <276629352@qq.com>
2019-08-04 18:02:18 +0800

    springboot2项目使用zookeeper

---
+++ springboot2-zookeeper/src/main/java/com/cxp/springboot2zookeeper/Springboot2ZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.cxp.springboot2zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2ZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2ZookeeperApplication.class, args);
+    }
+
+}
