726f385ac5c9d4e2ebb4ec0aab1c8b38fc1cc8ea
cheng <276629352@qq.com>
2019-08-04 18:03:05 +0800

    springboot2项目使用多数据源

---
+++ springboot2-multidatasource/src/main/java/com/cxp/springboot2multidatasource/Springboot2MultidatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.cxp.springboot2multidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2MultidatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2MultidatasourceApplication.class, args);
+    }
+
+}
