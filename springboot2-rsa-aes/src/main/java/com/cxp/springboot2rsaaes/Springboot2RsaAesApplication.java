5e0c5ad7944e8a61b0f4fa102ef6e539b37a2dde
cheng <276629352@qq.com>
2019-08-04 18:04:28 +0800

    springboot2项目使用rsa,aes对称和非对称加密传输数据

---
+++ springboot2-rsa-aes/src/main/java/com/cxp/springboot2rsaaes/Springboot2RsaAesApplication.java
@@ -0,0 +1,13 @@
+package com.cxp.springboot2rsaaes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2RsaAesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2RsaAesApplication.class, args);
+    }
+
+}
