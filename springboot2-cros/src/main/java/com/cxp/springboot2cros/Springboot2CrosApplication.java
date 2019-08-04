de8f06145e8719e8f56bf57eba25f5fc38b4a3b1
cheng <276629352@qq.com>
2019-08-04 18:01:21 +0800

    springboot2项目使用cros

---
+++ springboot2-cros/src/main/java/com/cxp/springboot2cros/Springboot2CrosApplication.java
@@ -0,0 +1,13 @@
+package com.cxp.springboot2cros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2CrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2CrosApplication.class, args);
+    }
+
+}
