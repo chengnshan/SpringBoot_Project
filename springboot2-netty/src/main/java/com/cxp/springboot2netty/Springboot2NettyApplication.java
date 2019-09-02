55e932cb7b28ce8b32bca734a0d60ee50408a12a
cheng <276629352@qq.com>
2019-09-02 19:48:16 +0800

    springboot2中使用netty

---
+++ springboot2-netty/src/main/java/com/cxp/springboot2netty/Springboot2NettyApplication.java
@@ -0,0 +1,13 @@
+package com.cxp.springboot2netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2NettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2NettyApplication.class, args);
+    }
+
+}
