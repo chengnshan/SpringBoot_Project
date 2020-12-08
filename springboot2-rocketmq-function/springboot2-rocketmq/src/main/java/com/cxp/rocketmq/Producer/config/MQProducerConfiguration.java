7f41a6e0f86a993cf3d210f2fb9a37e68152fd1d
cheng <276629352@qq.com>
2020-12-08 23:01:45 +0800

    springboot2-rocketmq

---
+++ springboot2-rocketmq-function/springboot2-rocketmq/src/main/java/com/cxp/rocketmq/Producer/config/MQProducerConfiguration.java
@@ -0,0 +1,74 @@
+package com.cxp.rocketmq.Producer.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.StringUtils;
+
+/**
+ * 生产者配置
+ */
+@Log4j2
+@SpringBootConfiguration
+public class MQProducerConfiguration {
+
+    /**
+     * 发送同一类消息的设置为同一个group，保证唯一,默认不需要设置，rocketmq会使用ip@pid(pid代表jvm名字)作为唯一标示
+     */
+    @Value("${rocketmq.producer.groupName}")
+    private String groupName;
+    @Value("${rocketmq.producer.namesrvAddr}")
+    private String namesrvAddr;
+    /**
+     * 消息最大大小，默认4M
+     */
+    @Value("${rocketmq.producer.maxMessageSize}")
+    private Integer maxMessageSize ;
+    /**
+     * 消息发送超时时间，默认3秒
+     */
+    @Value("${rocketmq.producer.sendMsgTimeout}")
+    private Integer sendMsgTimeout;
+    /**
+     * 消息发送失败重试次数，默认2次
+     */
+    @Value("${rocketmq.producer.retryTimesWhenSendFailed}")
+    private Integer retryTimesWhenSendFailed;
+
+    @Bean
+    public DefaultMQProducer getRocketMQProducer() throws MQClientException {
+        if (StringUtils.isEmpty(this.groupName)) {
+            throw new MQClientException(405, "groupName is blank");
+        }
+        if (StringUtils.isEmpty(this.namesrvAddr)) {
+            throw new MQClientException(405, "nameServerAddr is blank");
+        }
+        DefaultMQProducer producer;
+        producer = new DefaultMQProducer(this.groupName);
+        producer.setNamesrvAddr(this.namesrvAddr);
+        //如果需要同一个jvm中不同的producer往不同的mq集群发送消息，需要设置不同的instanceName
+        //producer.setInstanceName(instanceName);
+        if(this.maxMessageSize!=null){
+            producer.setMaxMessageSize(this.maxMessageSize);
+        }
+        if(this.sendMsgTimeout!=null){
+            producer.setSendMsgTimeout(this.sendMsgTimeout);
+        }
+        //如果发送消息失败，设置重试次数，默认为2次
+        if(this.retryTimesWhenSendFailed!=null){
+            producer.setRetryTimesWhenSendFailed(this.retryTimesWhenSendFailed);
+        }
+
+        try {
+            producer.start();
+            log.info("producer is start ! groupName:{},namesrvAddr:{}", this.groupName, this.namesrvAddr);
+        } catch (MQClientException e) {
+            log.error(String.format("producer is error [%s]", e.getMessage()),e);
+            throw new MQClientException("start MQClientException", e);
+        }
+        return producer;
+    }
+}
---
+++ springboot2-rocketmq-function/springboot2-rocketmq/src/main/java/com/cxp/rocketmq/SpringBoot2RocketmqApp.java
@@ -0,0 +1,11 @@
+package com.cxp.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2RocketmqApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2RocketmqApp.class, args);
+    }
+}
